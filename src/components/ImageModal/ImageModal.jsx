5fb52b607fafa2473be08e22ca6ac90722894c2f
Anastasia_S <sivokonanastasia1@gmail.com>
2025-02-02 21:37:41 +0200

    ain

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,35 +1,22 @@
 import Modal from "react-modal";
-import s from './ImageModal.module.css';
+import s from "./ImageModal.module.css";
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 const ImageModal = ({ modalIsOpen, closeModal, targetImage }) => {
-    const customStyles = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-    },
-  };
-    return (
-        <div>
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                styles={customStyles}
-                contentLabel="Modal opened"
-                className={s.Modal}
-                overlayClassName={s.Overlay}
-            >
-                <div>
-                    <img
-                        src={targetImage} alt="Large viw" className="s.modalImage"/>
-                </div>
-            </Modal>
-        </div>
-    );
-}
-export default ImageModal
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      contentLabel="Modal opened"
+      className={s.Modal}
+      overlayClassName={s.Overlay}
+    >
+      <div className={s.modalContent}>
+        <img src={targetImage} alt="Large view" className={s.modalImage} />
+      </div>
+    </Modal>
+  );
+};
+
+export default ImageModal;
